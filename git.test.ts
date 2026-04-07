edd4453412d4a6d93e97a107804044d626a71f85
Patrick Gaskin <patrick@pgaskin.net>
2026-04-06 22:08:44 -0400

    rewrite git stuff in typescript

---
+++ git.test.ts
@@ -0,0 +1,149 @@
+import { suite, describe, it } from 'node:test'
+import { equal } from 'node:assert'
+import * as git from './git.ts'
+
+suite('git', () => {
+    describe('splitCommitMessage', () => {
+        const test = (tc: {
+            what: string,
+            message: string,
+            subject: string,
+            body: string,
+        }) => {
+            it(tc.what, () => {
+                const {subject, body} = git.splitCommitMessage(tc.message)
+                equal(subject, tc.subject, 'subject')
+                equal(body, tc.body, 'body')
+            })
+        }
+        test({
+            what:    'empty',
+            message: '',
+            subject: '',
+            body:    '',
+        })
+        test({
+            what:    'subject only',
+            message: 'subject',
+            subject: 'subject',
+            body:    '',
+        })
+        test({
+            what:    'subject and body',
+            message: 'subject\n\nbody',
+            subject: 'subject',
+            body:    'body',
+        })
+        test({
+            what:    'two-line subject and body',
+            message: 'subject\nsubject2\n\nbody\nbody2',
+            subject: 'subject\nsubject2',
+            body:    'body\nbody2',
+        })
+        test({
+            what:    'three-line subject and body',
+            message: 'subject\nsubject2\nsubject3\n\nbody\nbody2\nbody3',
+            subject: 'subject\nsubject2\nsubject3',
+            body:    'body\nbody2\nbody3',
+        })
+        test({
+            what:    'three-line subject and body with insignificant newlines',
+            message: '\n\nsubject\nsubject2\nsubject3\n\n\n\nbody\nbody2\nbody3\n\n',
+            subject: 'subject\nsubject2\nsubject3',
+            body:    'body\nbody2\nbody3',
+        })
+        test({
+            what:    'three-line subject and body with insignificant newlines and whitespace',
+            message: '  \t \n   \nsubject\nsubject2\nsubject3\n \f  \n  \v \n\nbody\nbody2\nbody3\n \r  \n',
+            subject: 'subject\nsubject2\nsubject3',
+            body:    'body\nbody2\nbody3',
+        })
+        test({
+            what:    'three-line subject and body with insignificant newlines and whitespace, and significant whitespace',
+            message: '  \t \n   \nsubject\n   subject2\nsubject3\n \f  \n  \v \n\nbody\n  body2\nbody3\n \r  \n',
+            subject: 'subject\n   subject2\nsubject3',
+            body:    'body\n  body2\nbody3',
+        })
+    })
+    describe('trimBlankLinesStart', () => {
+        const test = (tc: [
+            string, // what
+            string, // in
+            string, // out
+        ]) => {
+            it(tc[0], () => {
+                const out = git.__test.trimBlankLinesStart(tc[1])
+                equal(out, tc[2])
+            })
+            test(['empty', '', ''])
+            test(['one newline', '\n', ''])
+            test(['all newline', '\n\n\n', ''])
+            test(['no newline', 'test', 'test'])
+            test(['no newline all spaces', '   ', '   '])
+            test(['no blank lines', 'test\ntest', 'test\ntest'])
+            test(['leading and trailing newline', '\ntest\ntest\n', 'test\ntest\n'])
+            test(['leading and trailing newlines', '\n\ntest\ntest\n\n', 'test\ntest\n\n'])
+            test(['ascii whitespace', '\n \t\r\v \ntest\n \t\r\v \n', 'test\n \t\r\v \n'])
+            test(['blank lines in middle', 'test\n\n\ntest', 'test\n\n\ntest'])
+            test(['blank line at start', '\ntest\n', 'test\n'])
+            test(['blank line in middle', 'test\n\ntest\n', 'test\n\ntest\n'])
+            test(['blank line at end', 'test\n\n', 'test\n\n'])
+            test(['blank line with ascii whitespace', 'test\n \t\r\v \ntest', 'test\n \t\r\v \ntest'])
+        }
+    })
+    describe('trimBlankLinesEnd', () => {
+        const test = (tc: [
+            string, // what
+            string, // in
+            string, // out
+        ]) => {
+            it(tc[0], () => {
+                const out = git.__test.trimBlankLinesEnd(tc[1])
+                equal(out, tc[2])
+            })
+            test(['empty', '', ''])
+            test(['one newline', '\n', ''])
+            test(['all newline', '\n\n\n', ''])
+            test(['no newline', 'test', 'test'])
+            test(['no newline all spaces', '   ', '   '])
+            test(['no blank lines', 'test\ntest', 'test\ntest'])
+            test(['leading and trailing newline', '\ntest\ntest\n', '\ntest\ntest'])
+            test(['leading and trailing newlines', '\n\ntest\ntest\n\n', '\n\ntest\ntest'])
+            test(['ascii whitespace', '\n \t\r\v \ntest\n \t\r\v \n', '\n \t\r\v \ntest'])
+            test(['blank lines in middle', 'test\n\n\ntest', 'test\n\n\ntest'])
+            test(['blank line at start', '\ntest\n', '\ntest'])
+            test(['blank line in middle', 'test\n\ntest\n', 'test\n\ntest'])
+            test(['blank line at end', 'test\n\n', 'test'])
+            test(['blank line with ascii whitespace', 'test\n \t\r\v \ntest', 'test\n \t\r\v \ntest'])
+        }
+    })
+    describe('cutBlankLine', () => {
+        const test = (tc: [
+            string, // what
+            string, // in
+            string, // before
+            string, // after
+        ]) => {
+            it(tc[0], () => {
+                const [before, after] = git.__test.cutBlankLine(tc[1])
+                equal(before, tc[2], 'before')
+                equal(after, tc[3], 'after')
+            })
+        }
+        test(['empty', '', '', ''])
+        test(['one newline', '\n', '', ''])
+        test(['all newline', '\n\n\n', '', '\n\n'])
+        test(['no newline', 'test', 'test', ''])
+        test(['no newline all spaces', '   ', '   ', ''])
+        test(['no blank lines', 'test\ntest', 'test\ntest', ''])
+        test(['leading and trailing newline', '\ntest\ntest\n', '', 'test\ntest\n'])
+        test(['leading and trailing newlines', '\n\ntest\ntest\n\n', '', '\ntest\ntest\n\n'])
+        test(['ascii whitespace', '\n \t\r\v \ntest\n \t\r\v \n', '', ' \t\r\v \ntest\n \t\r\v \n'])
+        test(['blank lines in middle', 'test\n\n\ntest', 'test\n', '\ntest'])
+        test(['blank line at start', '\ntest\n', '', 'test\n'])
+        test(['blank line in middle', 'test\n\ntest\n', 'test\n', 'test\n'])
+        test(['blank line at end', 'test\n\n', 'test\n', ''])
+        test(['blank line with ascii whitespace', 'test\n \t\r\v \ntest', 'test\n', 'test'])
+    })
+})
+
---
+++ git.ts
@@ -0,0 +1,418 @@
+import { spawn } from 'node:child_process'
+
+/** Git object types from object.h. */
+export const objectType = {
+  1: 'commit',
+  2: 'tree',
+  3: 'blob',
+  4: 'tag',
+  6: 'ofs_delta',
+  7: 'ref_delta',
+} as const
+
+/** Git object type name. */
+export type GitObjectType = typeof objectType[keyof typeof objectType]
+
+/** Used for nominal typing only. Not an actual property. */
+const __objectType = Symbol("git object")
+
+/** Git object hash. */
+export type TypedOID<T extends GitObjectType> = string & { readonly [__objectType]: T }
+export type OIDType<T extends TypedOID<GitObjectType>> = T[typeof __objectType]
+export type OID = TypedOID<GitObjectType>
+export type TreeOID = TypedOID<'tree'>
+export type CommitOID = TypedOID<'commit'>
+export type BlobOID = TypedOID<'blob'>
+export type TagOID = TypedOID<'tag'>
+
+/** OIDs peelable to a tree. */
+export type TreeishOID = TypedOID<'tree' | 'commit' | 'blob'>
+
+/** OIDs peelable to a commit. */
+export type CommittishOID = TypedOID<'commit' | 'tag'>
+
+/** An incomplete subset of revisions guaranteed to be treeish if they exist. */
+export type Treeish = "HEAD" | PeeledRev<OIDType<TreeishOID>> | TreeishOID
+
+/** An incomplete subset of revisions guaranteed to be committish if they exist. */
+export type Committish = "HEAD" | PeeledRev<OIDType<CommittishOID>> | CommittishOID
+
+/** An explicitly peeled revision (git will treat is as not found if not the expected object type). */
+export type PeeledRev<T extends GitObjectType> = `${string}^{${T}}`
+
+/** Returns revision peeled to the specified type. */
+export function peeledRev<T extends GitObjectType>(revision: string, type: T): PeeledRev<T> {
+  return `${revision}^{${type}}`
+}
+
+/** Git diff status character. */
+export type GitDiffStatus = typeof diffStatus[keyof typeof diffStatus]
+
+/** Git DIFF_STATUS_ constants from diff.h, but flipped for convenience. */
+export const diffStatus = {
+  added: 'A',
+  copied: 'C',
+  deleted: 'D',
+  modified: 'M',
+  renamed: 'R',
+  typeChanged: 'T',
+  unknown: 'X',
+  unmerged: 'U',
+} as const
+
+// I might have gone a bit crazy with the typing here, but it was fun, I learned
+// a bit, and now correctness is enforced ¯\_(ツ)_/¯
+//
+// I've chosen to make the types a bit stronger than strictly required, but I
+// think it's nicer this way
+
+export const [minGitMajor, minGitMinor] = [2, 38]
+
+export class GitParseError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'GitParseError'
+  }
+}
+
+export async function version(git: string): Promise<string> {
+  const out = await run(false, git, 'version')
+  const line = out.next()
+  if (line.done) {
+    throw new GitParseError(`Bad git version output ${out}`)
+  }
+  const match = /^git version (\S+)$/.exec(line.value)
+  if (!match) {
+    throw new GitParseError(`Bad git version line ${line.value}`)
+  }
+  return match[1]
+}
+
+export async function checkVersion(git: string): Promise<boolean | undefined> {
+  const ver = await version(git)
+  const match = /^(\d+)[.](\d+)[.](\d+)$/.exec(ver)
+  if (match) {
+    const major = parseInt(match[1])
+    const minor = parseInt(match[2])
+    return major > minGitMajor || (major == minGitMajor && minor >= minGitMinor)
+  }
+  return
+}
+
+export async function head(git: string): Promise<CommitOID> {
+  const out = await run(false, git, 'rev-parse', '--verify', 'HEAD')
+  for (const oid of out) {
+    parseOID<CommitOID>(oid)
+    return oid
+  }
+  throw new GitParseError(`Expected oid for successful rev-parse, got nothing`)
+}
+
+export async function commits(git: string, revision: string): Promise<CommitOID[]> {
+  const out = await run(false, git,
+    'rev-list',         // verify revs, list commits between them, and resolve them to their commit hash
+    '-z',               // null-terminated output
+    '--no-walk',        // if a single rev is specified, only resolve that one; ignored if a range is specified
+    '--first-parent',   // only follow the first parent of merge commits (we'll filter those out later anyways)
+    '--end-of-options', // prevent rev from being parsed as an option
+    revision,           // rev
+    '--',               // prevent rev from being parsed as a path
+  )
+  const oids = []
+  for (const oid of out) {
+    parseOID<CommitOID>(oid)
+    oids.push(oid)
+  }
+  return oids
+}
+
+export async function parents(git: string, commit: Committish): Promise<CommitOID[]> {
+  const out = await run(false, git, 'rev-parse', commit + '^@') // unlike sha^, this will not fail if a commit has no parents
+  const oids = []
+  for (const oid of out) {
+    parseOID<CommitOID>(oid)
+    oids.push(oid)
+  }
+  return oids
+}
+
+export async function message(git: string, commit: Committish): Promise<string> {
+  const out = await run(false, git,
+    '-c', 'i18n.logOutputEncoding=UTF-8', // if the commit message is not UTF-8, re-encode it
+    'show',                               // show a formatted object
+    '-s',                                 // only what we ask for, not the entire diff
+    '--format=%B',                        // raw commit message
+    '--end-of-options',                   // no more options
+    commit,                               // commit
+  )
+  return out.all()
+}
+
+export type GitDiffEntry = {
+  status: GitDiffStatus,
+  path: string,
+}
+
+export async function stagedDiff(git: string, tree: Treeish): Promise<GitDiffEntry[]> {
+  const out = await run(false, git,
+    'diff-index',       // low-level tree diff
+    '-z',               // null-terminated
+    '-r',               // recurse into trees (and don't return the trees themselves)
+    '--name-status',    // only status and paths
+    '--cached',         // only index (i.e.,  staging area), not working tree files
+    '--end-of-options', // no more options
+    tree,               // target
+  )
+  return parseDiffTree(out)
+}
+
+export async function commitDiff(git: string, a: Treeish, b: Treeish): Promise<GitDiffEntry[]> {
+  const out = await run(false, git,
+    'diff-tree',        // low-level tree diff
+    '-z',               // null-terminated
+    '-r',               // recurse into trees (and don't return the trees themselves)
+    '--name-status',    // only status and paths
+    '--end-of-options', // no more options
+    a, b,               // trees
+  )
+  return parseDiffTree(out)
+}
+
+async function parseDiffTree(out: GitOutput): Promise<GitDiffEntry[]> {
+  const diff = []
+  for (const status of out) {
+    const path = out.next()
+    if (path.done) {
+      throw new GitParseError(json`Expected path after diff status ${status}`)
+    }
+    parseDiffStatus(status, path.value)
+    diff.push({
+      status: status,
+      path: path.value,
+    })
+  }
+  return diff
+}
+
+export type GitTreeEntry = {
+  [T in GitObjectType]: {
+    type: T,
+    mode: number,
+    name: TypedOID<T>,
+    size: number,
+    path: string,
+  }
+}[GitObjectType]
+
+export async function listIndex(git: string, path: string): Promise<GitTreeEntry[]> {
+  const out = await run(false, git,
+    'ls-files',                    // information about a tree object in the index and working directory
+    '-z',                          // null terminated
+    '--cached',                    // only index (i.e.,  staging area), not working tree files
+    `--format=${parseTreeFormat}`, // fields
+    '--end-of-options',            // escape
+    path,                          // path
+  )
+  return parseTree(out)
+}
+
+export async function listTree(git: string, tree: TreeishOID, path: string): Promise<GitTreeEntry[]> {
+  const out = await run(false, git,
+    'ls-tree',                     // information about a tree object in the repository
+    '-z',                          // null terminated
+    `--format=${parseTreeFormat}`, // fields
+    '--end-of-options',            // escape
+    tree,                          // tree object
+    path,                          // path
+  )
+  return parseTree(out)
+}
+
+const parseTreeFormat = `%(objecttype)%x00%(objectmode)%x00%(objectname)%x00%(objectsize)%x00%(path)`
+
+function parseTree(out: GitOutput): GitTreeEntry[] {
+  const ent = []
+  for (const type of out) {
+    parseType(type)
+    const modeStr = out.next()
+    if (modeStr.done) {
+      throw new GitParseError(json`Expected mode after tree entry type ${type}`)
+    }
+    const mode = parseInt(modeStr.value, 10)
+    if (isNaN(mode) || !Number.isInteger(mode)) {
+      throw new GitParseError(json`Invalid mode ${modeStr}`)
+    }
+    const name = out.next()
+    if (name.done) {
+      throw new GitParseError(json`Expected mode after tree entry mode ${modeStr}`)
+    }
+    parseOID<TypedOID<any>>(name.value)
+    const sizeStr = out.next()
+    if (sizeStr.done) {
+      throw new GitParseError(json`Expected mode after tree entry name ${name}`)
+    }
+    const size = parseInt(sizeStr.value, 10)
+    if (isNaN(size) || !Number.isInteger(size)) {
+      throw new GitParseError(json`Invalid size ${modeStr}`)
+    }
+    const path = out.next()
+    if (path.done) {
+      throw new GitParseError(json`Expected mode after tree size ${sizeStr}`)
+    }
+    ent.push({
+      type: type,
+      mode: mode,
+      name: name.value,
+      size: size,
+      path: path.value,
+    })
+  }
+  return ent
+}
+
+export async function catFile(git: string, oid: OID): Promise<Buffer> {
+  return await run(true, git, 'cat-file', '-p', '--end-of-options', oid)
+}
+
+interface GitOutput extends IteratorObject<string, void, void> {
+  /** Get the next newline/null-delimited (depending on -z) item. */
+  next(): IteratorResult<string, void>
+  /** Get the entire UTF-8 output. */
+  all(): string
+}
+
+function run<T extends boolean>(raw: T, git: string, ...args: string[]): Promise<T extends true ? Buffer : GitOutput> {
+  return new Promise((resolve, reject) => {
+    const child = spawn(git, args)
+    const stdout: Buffer[] = []
+    const stderr: Buffer[] = []
+    child.stdout.on('data', chunk => stdout.push(chunk))
+    child.stderr.on('data', chunk => stderr.push(chunk))
+    child.on('error', err => reject(err))
+    child.on('close', (code, signal) => {
+      if (code) reject(new Error(json`git ${args}: exit status ${code} (stderr: ${Buffer.concat(stderr).toString('utf-8')})`))
+      else if (signal) reject(new Error(`git ${args}: killed by signal ${signal} (stderr: ${Buffer.concat(stderr).toString('utf-8')})`))
+      let out: any = Buffer.concat(stdout)
+      if (!raw) {
+        const all = out.toString('utf-8')
+        out = function* (str) {
+          const delim = args.includes('-z') ? '\x00' : '\n'
+          while (str.length) {
+            const i = str.indexOf(delim)
+            if (i == -1) {
+              throw new GitParseError(json`Got garbage ${str} after last ${delim}`)
+            }
+            const it = str.slice(0, i)
+            str = str.slice(i + 1)
+            yield it
+          }
+        }(all)
+        out = Object.defineProperty(out, 'all', {
+          value: () => all,
+          enumerable: false,
+          writable: true,
+          configurable: true,
+        })
+      }
+      resolve(out)
+    })
+  })
+}
+
+const diffStatusSet: Set<string> = new Set(Object.values(diffStatus))
+
+function parseDiffStatus(status: string, path?: string | undefined): asserts status is GitDiffStatus {
+  if (!diffStatusSet.has(status)) {
+    throw new GitParseError(json`Invalid diff status ${status}` + (path ? json`for file ${path}` : ''))
+  }
+}
+
+const objectTypeSet: Set<string> = new Set(Object.values(objectType))
+
+function parseType(type: string, path?: string | undefined): asserts type is GitObjectType {
+  if (!objectTypeSet.has(type)) {
+    throw new GitParseError(json`Invalid object type ${type}` + (path ? json`for file ${path}` : ''))
+  }
+}
+
+function parseOID<T extends OID>(oid: string): asserts oid is T {
+  if (!/^[a-f0-9]+$/.test(oid)) {
+    throw new GitParseError(json`Invalid OID ${oid}`)
+  }
+  if (oid.length != 40 && oid.length != 64) {
+    throw new GitParseError(json`Invalid OID ${oid} length ${oid.length}`)
+  }
+}
+
+/**
+ * Split message into the subject and body for pretty-printing according to
+ * git's rules (see git/pretty.c format_subject), does NOT merge the subject
+ * into a single line (so subject isn't exactly equal to --format=%s).
+ */
+export function splitCommitMessage(message: string): {
+  subject: string,
+  body: string,
+} {
+  message = trimBlankLinesStart(message)
+  let [subject, body] = cutBlankLine(message)
+  subject = trimBlankLinesEnd(subject)
+  body = trimBlankLinesStart(body)
+  body = trimBlankLinesEnd(body)
+  return {subject, body}
+}
+
+function trimBlankLinesStart(s: string): string {
+  while (true) {
+    const i = s.indexOf('\n')
+    if (i == -1) {
+      return s
+    }
+    if (!isSpaceASCII(s.slice(0, i))) {
+      return s
+    }
+    s = s.slice(i + 1)
+  }
+}
+
+function trimBlankLinesEnd(s: string): string {
+  while (true) {
+    const i = s.lastIndexOf('\n')
+    if (i == -1) {
+      return s
+    }
+    if (!isSpaceASCII(s.slice(i + 1))) {
+      return s
+    }
+    s = s.slice(0, i)
+  }
+}
+
+function cutBlankLine(s: string): [string, string] {
+  let rest = s
+  while (true) {
+    const i = rest.indexOf('\n')
+    if (i == -1) {
+      return [s, '']
+    }
+    if (isSpaceASCII(rest.slice(0, i))) {
+      return [s.slice(0, s.length - rest.length), rest.slice(i + 1)]
+    }
+    rest = rest.slice(i + 1)
+  }
+}
+
+function isSpaceASCII(s: string): boolean {
+  return /^[ \t\n\v\f\r]*$/.test(s)
+}
+
+export const __test = {
+  trimBlankLinesStart,
+  trimBlankLinesEnd,
+  cutBlankLine,
+}
+
+function json(strings: TemplateStringsArray, ...values: any[]) {
+  return strings.reduce((acc, str, i) => acc + str + (i < values.length ? JSON.stringify(values[i]) : ''), '');
+}
+
+console.log(isSpaceASCII("\t\r\x0B \n"))
