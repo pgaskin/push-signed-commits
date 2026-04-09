c6472388bb12b561bad8987c466c0140329aec3b
Patrick Gaskin <patrick@pgaskin.net>
2026-04-08 22:54:28 -0400

    wip

--- lib/action.ts
+++ lib/action.ts
@@ -1,32 +1,121 @@
+import type { GitHubApiUrl, GitHubGraphqlUrl } from './github.ts'
 import { EOL } from 'node:os'
 import { Console } from 'node:console'
+import { randomUUID } from 'node:crypto'
+import { appendFileSync, existsSync, readFileSync } from 'node:fs'
+import { repo } from './git.ts'
+import { NotPushableError, commits, staged } from './commit.ts'
+import {
+  DefaultGitHubApi, DefaultGitHubGraphql,
+  appJwt, getRepoInstallation, createInstallationToken, revokeInstallationToken,
+  createCommitOnBranch,
+} from './github.ts'
 
-globalThis.console = new Console({
-  stdout: process.stdout,
-  stderr: process.stderr,
-  colorMode: true,
-})
+if (import.meta.main) {
+  globalThis.console = new Console({
+    stdout: process.stdout,
+    stderr: process.stderr,
+    colorMode: true,
+  })
+  await main()
+}
 
-/**
- * Create an expandable group in the output for the current scope.
- */
-export function ghaGroup(name: string): Disposable {
-  ghaCommand('group', {}, name)
-  return {
-    [Symbol.dispose]() {
-      ghaCommand('endgroup', {}, '')
+export async function main(): Promise<void> {
+  const cfg = {
+    path: getInput('path') || '.',
+    repository: getInput('repository'),
+    branch: getInput('branch'),
+    revision: getInput('revision'),
+    allowEmpty: getBoolInput('allow-empty') ?? false,
+    commitMessage: getFileInput('commit-message-file') ?? getInput('commit-message'),
+    userAgent: getInput('user-agent'),
+    insecureSkipVerify: getBoolInput('insecure-skip-verify') ?? false,
+    dryRun: getBoolInput('dry-run') ?? false,
+    githubToken: getInput('github-token'),
+    githubApiUrl: getUrlInput('github-api-url') as GitHubApiUrl || DefaultGitHubApi,
+    githubGraphqlUrl: getUrlInput('github-graphql-url') as GitHubGraphqlUrl || DefaultGitHubGraphql,
+    appId: getInput('app-id'),
+    appKey: getInput('app-key'),
+    gitBinary: getInput('git-binary') || 'git',
+  } as const
+
+  if (cfg.insecureSkipVerify) {
+    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
+  }
+
+  const r = await repo(cfg.gitBinary, cfg.path)
+  
+}
+
+function getUrlInput(name: string): string | undefined {
+  const str = getInput(name)
+  if (str !== '') {
+    try {
+      const u = new URL(str)
+      if (u.protocol === '' || u.host === '') {
+        throw new Error('Missing protocol/host')
+      }
+      return str
+    } catch (err) {
+      throw new Error(`Input ${name}: ${err}`)
     }
+  }
+  return
+}
+
+function getFileInput(name: string): string | undefined {
+  const str = getInput(name)
+  if (str !== '') {
+    try {
+      return readFileSync(str, { encoding: 'utf-8' })
+    } catch (err) {
+      throw new Error(`Input ${name}: ${err}`)
+    }
+  }
+  return
+}
+
+function getBoolInput(name: string): boolean | undefined {
+  const str = getInput(name)
+  if (str !== '') {
+    switch (str) {
+      case '1': case 't': case 'T': case 'true': case 'TRUE': case 'True':
+        return true
+      case '0': case 'f': case 'F': case 'false': case 'FALSE': case 'False':
+        return false
+    }
+    throw new Error(`Input ${name}: ${JSON.stringify(str)} is not a valid bool`)
+  }
+  return
+}
+
+// https://docs.github.com/en/actions/reference/workflows-and-actions/workflow-commands
+
+// actions/core@v3.0.0/src/core.ts, but simpler
+export function getInput(name: string): string {
+  const key = `INPUT_${name.replace(/ /g, '_').toUpperCase()}`
+  return process.env[key]?.trim() ?? ''
+}
+
+// actions/core@v3.0.0/src/core.ts, but simpler
+export function beginGroup(name: string): Disposable {
+  issueCommand('group', {}, name)
+  return { [Symbol.dispose]() { issueCommand('endgroup', {}, '') } }
+}
+
+// actions/core@v3.0.0/src/core.ts
+export const DEBUG = process.env['RUNNER_DEBUG'] === '1'
+
+// actions/core@v3.0.0/src/core.ts, but simpler
+export function setOutput(name: string, value: string): void {
+  if (!issueFileCommand('OUTPUT', name, value)) {
+    process.stdout.write(EOL)
+    issueCommand("set-output", { name }, value)
   }
 }
 
-/**
- * Emits a command for GitHub Actions.
- *
- * Based on logic in:
- * - actions/core@v3.0.0/src/command.ts
- * - https://docs.github.com/en/actions/reference/workflows-and-actions/workflow-commands
- */
-export function ghaCommand(command: string, properties: {[key: string]: any}, message: string) {
+// actions/core@v3.0.0/src/command.ts, but simpler
+export function issueCommand(command: string, properties: {[key: string]: any}, message: string): void {
   let props = ''
   if (properties) {
     for (const [key, val] of Object.entries(properties)) {
@@ -43,29 +132,19 @@ export function ghaCommand(command: string, properties: {[key: string]: any}, me
   process.stdout.write(`::${command}${props}::${message.replace(/%/g, '%25').replace(/\r/g, '%0D').replace(/\n/g, '%0A')}${EOL}`)
 }
 
-/**
- * Get an action input.
- *
- * Based on logic in:
- * - actions/core@v3.0.0/src/core.ts
- */
-export function getInput(name: string, trim: boolean = false): string {
-  const val: string = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || ''
-  return trim ? val.trim() : val
+// actions/core@v3.0.0/src/file-command.ts, but simpler
+export function issueFileCommand(command: string, key: string, value: string): boolean {
+  const path = process.env[`GITHUB_${command}`]
+  if (path) {
+    if (!existsSync(path)) {
+      throw new Error(`Missing ${command} command file ${path}`)
+    }
+    const delim = `ghadelimiter_${randomUUID()}`
+    if (key.includes(delim) || value.includes(delim)) {
+      throw new Error(`Key/value includes random delimiter ${delim}`)
+    }
+    appendFileSync(path, `${key}<<${delim}${EOL}${value}${EOL}${delim}${EOL}`, { encoding: 'utf8' })
+    return true
+  }
+  return false
 }
-
-/**
- * Whether we're in debug mode.
- *
- * Based on logic in:
- * - actions/core@v3.0.0/src/core.ts
- */
-export const DEBUG = process.env['RUNNER_DEBUG'] === '1'
-
-/**
- * Whether the runner is self-hosted.
- *
- * Based on logic in:
- * - actions/setup-go@v6.4.0/src/utils.ts
- */
-export const SELFHOSTED = process.env['RUNNER_ENVIRONMENT'] !== 'github-hosted' && (process.env['AGENT_ISSELFHOSTED'] === '1' || process.env['AGENT_ISSELFHOSTED'] === undefined)
