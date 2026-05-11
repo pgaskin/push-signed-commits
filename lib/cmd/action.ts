6de105c9474048d11eb23b4b7d97f7f753e9de06
Patrick Gaskin <patrick@pgaskin.net>
2026-05-11 00:43:06 -0400

    Fix debug logging when running as a GitHub Action

--- lib/cmd/action.ts
+++ lib/cmd/action.ts
@@ -42,7 +42,7 @@ export async function main(opts: {
     const log = (msg?: string) => msg ? console.log(msg) : console.log()
     setRetryLog(msg => log(styleText(['dim', 'yellow'], msg)))
     hookDebugLog((section, msg) => {
-      debug(`[${section}] msg`) // secret ACTIONS_STEP_DEBUG
+      debug(`[${section}] ${msg}`) // secret ACTIONS_STEP_DEBUG
       return msg
     })
     return await main_(log, inputs(opts.env), outputs)
