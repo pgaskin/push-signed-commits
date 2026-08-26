pgaskin/push-signed-commits

commit e7404cbeec2d1b73d69c64ccdf4fd5925fff7404

Support client-id for GitHub App authentication
GitHub apparently recommends this now: actions/create-github-app-token@e6bd4e6970172bed9fe138b2eaf4cbffa4cca8f9.

Since they both end up in the JWT iss as a string anyways, just reuse
the existing option for simplicity and to keep the CLI clean.

closes #1

lib/cmd/action.ts
@@ -13,7 +13,6 @@ import {
 import { debug, getInput, setOutput } from '../util/gha.ts'
 import {
   type Input, type Output, main as main_,
-  parseInteger,
   parsePrivateKey, validateBaseUrl,
 } from './main.ts'
 import { hookDebugLog, makeUserAgent } from '../util/util.ts'
@@ -72,7 +71,7 @@ export function inputs(env: NodeJS.ProcessEnv): Input {
     githubToken: getInput('github-token') as GitHubToken,
     githubApiUrl: getUrlInput('github-api-url') as GitHubApiUrl || env['GITHUB_API_URL'] as GitHubApiUrl || DefaultGitHubApi,
     githubGraphqlUrl: getUrlInput('github-graphql-url') as GitHubGraphqlUrl || env['GITHUB_GRAPHQL_URL'] as GitHubGraphqlUrl || DefaultGitHubGraphql,
-    appId: getIntegerInput('app-id') ?? null,
+    appId: getInput('app-id') ?? null,
     appKey: getKeyInput('app-key') ?? null,
     git: getInput('git-binary') || 'git',
   }
@@ -137,18 +136,6 @@ function getBoolInput(name: string): boolean | undefined {
         return false
     }
     throw new ActionInputError(name, `Invalid bool ${JSON.stringify(str)}`)
-  }
-  return
-}
-
-function getIntegerInput(name: string): number | undefined {
-  const str = getInput(name)
-  if (str !== '') {
-    try {
-      return parseInteger(str)
-    } catch (err) {
-      throw new ActionInputError(name, `${err instanceof Error ? err.message : err}`)
-    }
   }
   return
 }

action.yml
@@ -76,10 +76,11 @@ inputs:
       https://my-ghes-server.example.com/api/graphql).
   app-id:
     description:
-      Authenticate as a GitHub App with the specified ID. The installation ID
-      will be detected based on 'repository'. Overrides 'github-token'. The app
-      must have the 'contents:write' permission. If you already have an app
-      installation token, you can pass it via 'github-token' instead.
+      Authenticate as a GitHub App with the specified ID (App ID or Client ID).
+      The installation ID will be detected based on 'repository'. Overrides
+      'github-token'. The app must have the 'contents:write' permission. If you
+      already have an app installation token, you can pass it via 'github-token'
+      instead.
   app-key:
     description:
       The private key to use if authenticating as a GitHub App. Can be

lib/cmd/cli.ts
@@ -11,7 +11,7 @@ import {
 import { OptionError, parseOptions, help } from '../util/options.ts'
 import {
   type Input, main as main_,
-  parseInteger, parsePrivateKey, validateBaseUrl,
+  parsePrivateKey, validateBaseUrl,
 } from './main.ts'
 import { hookDebugLog, makeUserAgent } from '../util/util.ts'
 
@@ -71,7 +71,7 @@ const spec = {
   githubToken: { type: 'str', kind: 'token', long: 'github-token', env: 'GITHUB_TOKEN', help: 'github token with contents:write permission', parse: t => t ? t as GitHubToken : null }, // env is standard
   githubApiUrl: { type: 'str', kind: 'url', long: 'github-api-url', env: 'GITHUB_API_URL', help: 'github api url', default: DefaultGitHubApi, parse: u => (validateBaseUrl(u), u as GitHubApiUrl) }, // env is standard
   githubGraphqlUrl: { type: 'str', kind: 'url', long: 'github-grqphql-url', env: 'GITHUB_GRAPHQL_URL', help: 'github graphql api url', default: DefaultGitHubGraphql, parse: u => (validateBaseUrl(u), u as GitHubGraphqlUrl) }, // env is standard
-  appId: { type: 'str', kind: 'id', long: 'app', help: 'authenticate as a github app with the specified id (overrides --github-token)', parse: n => n ? parseInteger(n) : null },
+  appId: { type: 'str', kind: 'app-id|client-id', long: 'app', help: 'authenticate as a github app with the specified id (overrides --github-token)', parse: n => n || null },
   appKey: { type: 'str', kind: 'pem', long: 'app-key', env: 'APP_PRIVATE_KEY', help: 'the private key to use if authenticating as a github app (can be base64-encoded or contain escaped newlines)', parse: s => s ? parsePrivateKey(s) : null },
   git: { type: 'str', kind: 'cmd', long: 'git', help: 'the git executable to use', default: 'git' },
   help: { type: 'bool', short: 'h', long: 'help', help: 'show this help text' },

lib/cmd/main.ts
@@ -25,7 +25,7 @@ export interface Input {
   githubToken: GitHubToken | null,
   githubApiUrl: GitHubApiUrl,
   githubGraphqlUrl: GitHubGraphqlUrl,
-  appId: number | null,
+  appId: string | null,
   appKey: KeyObject | null,
   git: string,
 }
@@ -203,14 +203,6 @@ export function parsePrivateKey(str: string): KeyObject {
     key: str,
     format: 'pem',
   })
-}
-
-export function parseInteger(str: string): number {
-  const v = Number(str)
-  if (!Number.isInteger(v)) {
-    throw new TypeError(`Invalid integer`)
-  }
-  return v
 }
 
 export function validateBaseUrl(str: string) {

lib/core/github.ts
@@ -44,7 +44,7 @@ export function withUserAgent(token: GitHubToken, userAgent: string): GitHubToke
 }
 
 /** Creates a signed GitHub App JWT. */
-export function appJwt(appId: number, rsaKey: KeyObject): GitHubAppJwt {
+export function appJwt(appOrClientId: number | string, rsaKey: KeyObject): GitHubAppJwt {
   const header = Buffer.from(JSON.stringify({
     alg: 'RS256',
     typ: 'JWT',
@@ -54,7 +54,7 @@ export function appJwt(appId: number, rsaKey: KeyObject): GitHubAppJwt {
   const payload = Buffer.from(JSON.stringify({
     iat: now - 60,
     exp: now + 60,
-    iss: String(appId),
+    iss: String(appOrClientId),
   })).toString('base64url')
 
   const sign = createSign('RSA-SHA256')

commit 597d620104cbd37824db3e00c6373daa1b561652

Fix required private-key error when no app-id
The GHA getInput returns an empty string, not undefined, when not set.

Regression in e7404cbeec2d1b73d69c64ccdf4fd5925fff7404 since v1.2.0.

fixes #4

lib/cmd/action.ts
@@ -71,7 +71,7 @@ export function inputs(env: NodeJS.ProcessEnv): Input {
     githubToken: getInput('github-token') as GitHubToken,
     githubApiUrl: getUrlInput('github-api-url') as GitHubApiUrl || env['GITHUB_API_URL'] as GitHubApiUrl || DefaultGitHubApi,
     githubGraphqlUrl: getUrlInput('github-graphql-url') as GitHubGraphqlUrl || env['GITHUB_GRAPHQL_URL'] as GitHubGraphqlUrl || DefaultGitHubGraphql,
-    appId: getInput('app-id') ?? null,
+    appId: getInput('app-id') || null,
     appKey: getKeyInput('app-key') ?? null,
     git: getInput('git-binary') || 'git',
   }